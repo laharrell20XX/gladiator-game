6ef8baf5c78f60c5cf48802bd2e0c58cfac1c49c
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-07-09 16:12:35 -0500

    added function to show gladiator stats (user friendly)

--- shell.py
+++ shell.py
@@ -2,4 +2,13 @@ import core
 
 
 def gladiators():
-    gladiator1 = new_gladiator()
+    gladiator1 = new_gladiator(100, 0, 5, 15)
+    gladiator2 = new_gladiator(100, 0, 5, 15)
+
+
+def show_gladiators(gladiator1, gladiator2):
+    print('''
+Gladiator 1: {} HP ||| {} Rage
+Gladiator 2: {} HP ||| {} Rage'''
+          .format(gladiator1['health'], gladiator1['rage'],
+                  gladiator2['health'], gladiator2['rage']))
