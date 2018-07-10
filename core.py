a3c36ad352591bfc28e9313766e8406799100595
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-07-10 16:22:35 -0500

    added a defense function, and put it in the attack function

--- core.py
+++ core.py
@@ -1,13 +1,15 @@
 from random import randrange, randint
 
 
-def new_gladiator(gladiator_name, health, rage, damage_low, damage_high):
+def new_gladiator(gladiator_name, health, rage, damage_low, damage_high,
+                  defending):
     return dict(
         gladiator_name=gladiator_name,
         health=health,
         rage=rage,
         damage_low=damage_low,
-        damage_high=damage_high)
+        damage_high=damage_high,
+        defending=defending)
 
 
 def attack(attacker, defender):
@@ -16,27 +18,46 @@ def attack(attacker, defender):
     if crit_chance < attacker['rage']:  #the higher the rage, the bigger chance for a critical hit
         damage_dealt = damage_dealt * 2
         attacker['rage'] = 0
-        print('{} critical hit {} for {} damage!'.format(
-            attacker['gladiator_name'], defender['gladiator_name'],
-            damage_dealt))
+        if defender['defending']:
+            damage_dealt = defend(defender, damage_dealt)
+            print(
+                'Critical hit successfully blocked! {} only received {} damage'.
+                format(defender['gladiator_name'], damage_dealt))
+        else:
+            print('{} critically hit {} for {} damage!'.format(
+                attacker['gladiator_name'], defender['gladiator_name'],
+                damage_dealt))
         defender['health'] -= damage_dealt
     else:
-        defender['health'] = defender['health'] - damage_dealt
-        defender['rage'] += 15
+        if defender['defending']:
+            damage_dealt = defend(defender, damage_dealt)
+            print('Hit was blocked! {} only received {} damage'.format(
+                defender['gladiator_name'], damage_dealt))
+            attacker['rage'] = 0
+        else:
+            attacker['rage'] += 15
         print('{} hit {} for {} damage'.format(attacker['gladiator_name'],
                                                defender['gladiator_name'],
                                                damage_dealt))
+        defender['health'] -= damage_dealt
+
     if defender['health'] < 0:
         print('{} has been fatally wounded by {}'.format(
             defender['gladiator_name'], attacker['gladiator_name']))
         defender['health'] = 0
 
 
+def defend(attacker,
+           damage_dealt):  #should reduce the damage of the next hit by 50%
+    damage_dealt = damage_dealt * .50
+    return damage_dealt
+
+
 def heal(gladiator):
     if (gladiator['health'] + 5) > 100 and gladiator['health'] < 100:
         gladiator['rage'] -= 10
         gladiator['health'] = 100
-    elif (gladiator['health'] + 5) < 100:
+    elif (gladiator['health'] + 5) <= 100:
         gladiator['rage'] -= 10
         gladiator['health'] += 5
     else:
