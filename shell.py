7aeb18afb91da4a9a2ee83020e4cec486da7996e
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-07-10 16:35:09 -0500

    gladiators can now defend against each other

--- shell.py
+++ shell.py
@@ -1,8 +1,10 @@
 import core
+from random import randint, choice
 
 
 def gladiator(gladiator_name):
-    gladiator = core.new_gladiator(gladiator_name, 100, 0, 5, 15)
+    gladiator = core.new_gladiator(gladiator_name, 100, 0, randint(0, 5),
+                                   randint(6, 15), False)
     return gladiator
 
 
@@ -15,31 +17,35 @@ def show_gladiators(gladiator_1, gladiator_2):
                   gladiator_2['health'], gladiator_2['rage']))
 
 
-def gladiator_makes_move(gladiator_name):
+def gladiator_makes_move(attacker, defender):
     while True:
-        move = input('''{}... What would you like to do?
+        move = input('''{}... What would you like to do? {}
 - attack
 - pass
 - quit
 - heal
->>> '''.format(gladiator_name))
+>>> '''.format(attacker['gladiator_name'], attacker['defending']))
         if 'attack' == move:
             return move
         elif 'pass' == move:
             return move
         elif 'quit' == move:
-            return move
+            print('{}: Survived!\n{}: Survived!'.format(
+                defender['gladiator_name'], attacker['gladiator_name']))
+            quit()
         elif 'heal' == move:
             return move
-        #elif 'supah kick' == move:  Used to test death condititon
-        #    return move
-        #elif 'angery' == move:  Used to test critical hits
-        #    return move
+        elif 'defend' == move:
+            return move
+        elif 'supah kick' == move:  #Used to test death condititon
+            return move
+        elif 'angery' == move:  #Used to test critical hits
+            return move
         else:
             print('Invalid input')
 
 
-def gladiator_fight():
+def gladiator_fight():  #add defend counter
     gladiator_1 = gladiator('Gladiator 1')
     gladiator_2 = gladiator('Gladiator 2')
 
@@ -47,29 +53,28 @@ def gladiator_fight():
     # while attacker['health'] and defender['health']:
     while True:
         show_gladiators(attacker, defender)
-        gladiator1_move = gladiator_makes_move(attacker['gladiator_name'])
-        if gladiator1_move == 'attack':
+        gladiator_move = gladiator_makes_move(attacker, defender)
+        if gladiator_move == 'attack':
+            attacker['defending'] = False
             core.attack(attacker, defender)
-        #elif gladiator1_move == 'supah kick':
-        #    defender['health'] -= 99
-        #elif gladiator1_move == 'angery':
-        #    attacker['rage'] += 100
-        #    continue
-        elif gladiator1_move == 'heal':
+        elif gladiator_move == 'defend':
+            attacker['defending'] = True
+        elif gladiator_move == 'supah kick':  #negative  health test
+            defender['health'] -= 99
+        elif gladiator_move == 'angery':  #crit hit test
+            attacker['rage'] += 100
+            continue
+        elif gladiator_move == 'heal':
+            attacker['defending'] = False
             if attacker['rage'] < 10:
                 print("You try to heal, but you just aren't angry enough")
-                continue
             if attacker['health'] == 100:
                 print(
                     "You spend some time searching for wounds to self-treat, but no serious one's are found (Turn used up)"
                 )
             else:
                 core.heal(attacker)
-        elif gladiator1_move == 'quit':
-            print('{}: Survived!\n{}: Survived!'.format(
-                defender['gladiator_name'], attacker['gladiator_name']))
-            break
-        elif gladiator1_move == 'pass':
+        elif gladiator_move == 'pass':
             pass
         if core.is_dead(defender):
             print(
@@ -77,29 +82,6 @@ def gladiator_fight():
                     defender['gladiator_name'], attacker['gladiator_name']))
             break
         attacker, defender = defender, attacker  #uses multivariable assignment to switch between attacker and defender at the end of every while loop
-
-        # gladiator2_move = gladiator_makes_move(gladiator_2, 2)
-        # if gladiator2_move == 'attack':
-        #     core.attack(gladiator_2, gladiator_1)
-        # elif gladiator2_move == 'heal':
-        #     if gladiator_2['rage'] < 10:
-        #         print("You try to heal, but you just aren't angry enough")
-        #         continue
-        #     elif gladiator_2['health'] == 100:
-        #         print(
-        #             "You spend some time searching for wounds to self-treat, but no serious one's are found (Turn used up)"
-        #         )
-        #         continue
-        #     else:
-        #         core.heal(gladiator_2)
-        # elif gladiator1_move == 'quit':
-        #     print('Gladiator 1: Survived!\nGladiator 2: Survived!')
-        #     break
-        # elif gladiator1_move == 'pass':
-        #     pass
-        # if core.is_dead(gladiator_1):
-        #     print('Gladiator 2 has died. Funeral procession tommorrow')
-        #     break
 
 
 def main():
