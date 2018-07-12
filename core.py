99cda4c03598a65acac13c704e328f7116f74450
laharrell20XX <lharrell@basecampcodingacademy.org>
2018-07-12 11:22:16 -0500

    added a miss chance for the gladiators

--- core.py
+++ core.py
@@ -2,7 +2,8 @@ from random import randrange, randint
 
 
 def new_gladiator(gladiator_name, health, rage, damage_low, damage_high,
-                  defending, defense, stunned_turns, stunned_status):
+                  defending, defense, stunned_turns, stunned_status,
+                  miss_chance):
     return dict(
         gladiator_name=gladiator_name,
         health=health,
@@ -12,50 +13,65 @@ def new_gladiator(gladiator_name, health, rage, damage_low, damage_high,
         defending=defending,
         defense=defense,
         stunned_turns=stunned_turns,
-        stunned_status='')
+        stunned_status='',
+        miss_chance=miss_chance)
 
 
 def attack(attacker, defender):
     damage_dealt = randint(attacker['damage_low'], attacker['damage_high'])
-    crit_chance = randrange(1, 100)
+    crit_chance, miss_chance = randrange(1, 100), randrange(1, 100)
     if crit_chance < attacker['rage']:  #the higher the rage, the bigger chance for a critical hit
         damage_dealt = damage_dealt * 2
         attacker['rage'] = 0
-        if defender['defending']:
-            if defender['defense'] > 0:  # if defense is high enough to block a crit hit
-                damage_dealt = defend(defender, damage_dealt)
-                print(
-                    'Critical hit successfully blocked! {} only received {} damage'.
-                    format(defender['gladiator_name'], damage_dealt))
-            else:  #if defense is not high enough to block a crit hit
+        if miss_chance < attacker['miss_chance']:  #if the attacker misses
+            damage_dealt = 0
+            attacker['rage'] = 0
+            print(
+                "With a mighty swing, {} cleaves an innocent rock at {}'s feet. {} laughs.".
+                format(attacker['gladiator_name'], defender['gladiator_name'],
+                       defender['gladiator_name']))
+        else:
+            if defender['defending']:
+                if defender['defense'] > 0:  # if defense is high enough to block a crit hit
+                    damage_dealt = defend(defender, damage_dealt)
+                    print(
+                        'Critical hit successfully blocked! {} only received {} damage'.
+                        format(defender['gladiator_name'], damage_dealt))
+                else:  #if defense is not high enough to block a crit hit
+                    print('{} critically hit {} for {} damage!'.format(
+                        attacker['gladiator_name'], defender['gladiator_name'],
+                        damage_dealt))
+                defender['defense'] -= 2
+            else:
                 print('{} critically hit {} for {} damage!'.format(
                     attacker['gladiator_name'], defender['gladiator_name'],
                     damage_dealt))
-            defender['defense'] -= 2
-        else:
-            print('{} critically hit {} for {} damage!'.format(
-                attacker['gladiator_name'], defender['gladiator_name'],
-                damage_dealt))
     else:  #normal hit condition
-        if defender['defending']:
-            if defender['defense'] > 0:  # if defense is high enough to block a normal attack
-                damage_dealt = defend(defender, damage_dealt)
-                print('Hit was blocked! {} only received {} damage'.format(
-                    defender['gladiator_name'], damage_dealt))
-                attacker['rage'] = 0
-            else:  # if defense is not high enough to block a normal attack
+        if miss_chance < attacker['miss_chance']:
+            damage_dealt = 0
+            print(
+                "You try to attack your opponent, but you miss and scuff his shoes instead. 'Those were new!' {} exclaims".
+                format(defender['gladiator_name']))
+            defender['rage'] += 10
+        else:
+            if defender['defending']:
+                if defender['defense'] > 0:  # if defense is high enough to block a normal attack
+                    damage_dealt = defend(defender, damage_dealt)
+                    print('Hit was blocked! {} only received {} damage'.format(
+                        defender['gladiator_name'], damage_dealt))
+                    attacker['rage'] = 0
+                else:  # if defense is not high enough to block a normal attack
+                    attacker['rage'] += 15
+                    print('{} hit {} for {} damage'.format(
+                        attacker['gladiator_name'], defender['gladiator_name'],
+                        damage_dealt))
+                defender[
+                    'defense'] -= 1  #defense is always decreased if glad is defending
+            else:
                 attacker['rage'] += 15
                 print('{} hit {} for {} damage'.format(
                     attacker['gladiator_name'], defender['gladiator_name'],
                     damage_dealt))
-            defender[
-                'defense'] -= 1  #defense is always decreased if glad is defending
-        else:
-            attacker['rage'] += 15
-            print('{} hit {} for {} damage'.format(attacker['gladiator_name'],
-                                                   defender['gladiator_name'],
-                                                   damage_dealt))
-
     defender['health'] -= damage_dealt
     if defender['health'] < 0:
         print('{} has been fatally wounded by {}'.format(
